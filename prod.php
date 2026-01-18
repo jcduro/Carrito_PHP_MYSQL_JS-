ce39da373d29c39e1be8adac62d5217ec2122947
JUAN CARLOS <69373493+jcduro@users.noreply.github.com>
2026-01-18 11:42:49 -0500

    Create prod.php for product display page

---
+++ prod.php
@@ -0,0 +1,77 @@
+
+ <!-- Welcome to
+  
+     |  ___|  __ \  |   |  _ \   _ \   
+     | |      |   | |   | |   | |   |  
+ \   | |      |   | |   | __ <  |   |  
+\___/ \____| ____/ \___/ _| \_\\___/   
+                                       
+  ___|  _ \  __ \  ____|    _ )   _ _| __ \  ____|    \     ___|  
+ |     |   | |   | __|     _ \ \   |  |   | __|     _ \  \___ \  
+ |     |   | |   | |      ( `  <   |  |   | |      ___ \       | 
+\____|\___/ ____/ _____| \___/\/ ___|____/ _____|_/    _\_____/  
+
+  https://jcduro.bexartideas.com/index.php | 2026 | JC Duro Code & Ideas
+
+------------------------------------------------------------------------------- -->
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Productos</title>
+  <link rel="stylesheet" href="css/prod.css">
+</head>
+<body>
+  
+
+
+ 
+       <center>
+<main>
+    <!-- Icono carrito -->
+<div class="cart-toggle">
+  <span class="cart-icon">🛒</span>
+  <span class="cart-count">0</span>
+</div>
+
+<!-- Panel lateral carrito -->
+<div class="cart-sidebar" id="cartSidebar">
+  <h2>Carrito Neon</h2>
+  <div class="cart-items"></div>
+  <div class="cart-footer">
+    <span>Total:</span>
+    <span class="cart-total">$0</span>
+  </div>
+</div>
+
+
+  <div class="product-container">
+    <div class="list" id="product-list">
+      <!-- Aquí se inyectan los productos -->
+    </div>
+    <ul class="listPage"></ul>
+  </div>
+
+        <ul class="listPage">
+           
+        </ul>
+    </center>
+</main>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+
+         </main>
+
+
+  <script src="js/prod.js"></script>
+</body>
+</html>
+
+
